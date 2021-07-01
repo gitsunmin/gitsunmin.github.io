c0aaa34245493d72f51fefc9ef09b56db22f9f77
김선민 <tjsals116@gmail.com>
2021-07-01 17:36:10 +0900

    test npm install

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-// import cytoscape from 'cytoscape';
-const a = require('cytoscape');
+import cytoscape from 'cytoscape';
+// const a = require('cytoscape');
 
-console.log('이게 실행이 될까?', a);
+console.log('이게 실행이 될까?', cytoscape);
