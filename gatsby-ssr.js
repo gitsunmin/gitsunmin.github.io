79a81fc99e69d60664a108f667f5dbf7ad3bf33a
김선민 <tjsals116@gmail.com>
2022-07-19 02:21:23 +0900

    주석 제거

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -8,8 +8,6 @@ import 'prismjs/themes/prism.css';
 // normalize CSS across browsers
 import './src/styles/css/normalize.css';
 
-// import "./src/styles/css/style.css"
-
 // React, Recoil 적용
 import React from 'react';
 import { RecoilRoot } from 'recoil';
