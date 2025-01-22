9faffe436e51d0196ec12abf9cb6bb1e2b6a305b
김선민 <tjsals116@gmail.com>
2025-01-22 23:27:53 +0900

    ♻️ remove deprecated code on import.meta.glob

--- src/routes/til/$category.$slug.tsx
+++ src/routes/til/$category.$slug.tsx
@@ -5,7 +5,9 @@ export const Route = createFileRoute('/til/$category/$slug')({
   loader: async ({ params }) => {
     const { category, slug } = params;
     // 파일 목록을 가져오는 glob 설정
-    const modules = import.meta.glob('/modules/til/**/*.md', { as: 'raw' });
+    const modules = import.meta.glob('/modules/til/**/*.md', {
+      query: 'raw',
+    }) as Record<string, () => Promise<{ default: string }>>;
 
     // 동적 경로로 파일 찾기
     const filePath = `/modules/til/${category}/${slug}`;
@@ -15,7 +17,7 @@ export const Route = createFileRoute('/til/$category/$slug')({
     }
 
     // 해당 파일 로드
-    const content = await modules[filePath]();
+    const content = await modules[filePath]().then((res) => res.default);
 
     return {
       content,
@@ -26,10 +28,8 @@ export const Route = createFileRoute('/til/$category/$slug')({
 function RouteComponent() {
   const { content } = getRouteApi('/til/$category/$slug').useLoaderData();
   return (
-    <main className="mx-auto flex justify-center">
-      <section>
-        <Post content={content} />
-      </section>
+    <main>
+      <Post content={content} />
     </main>
   );
 }
