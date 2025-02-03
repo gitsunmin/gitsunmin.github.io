ba4b9bca2114155337e87e1f11e01f8770ff77ac
김선민 <tjsals116@gmail.com>
2025-02-03 23:17:51 +0900

    ✨ change main page layout

--- src/routes/index.lazy.tsx
+++ src/routes/index.lazy.tsx
@@ -1,5 +1,10 @@
 import { createLazyFileRoute, Link } from '@tanstack/react-router';
 
+import TILIcon from '@/assets/icons/menu_til.webp';
+import CareersIcon from '@/assets/icons/menu_careers.webp';
+import ProjectsIcon from '@/assets/icons/menu_projects.webp';
+import InterviewIcon from '@/assets/icons/menu_interview.webp';
+
 export const Route = createLazyFileRoute('/')({
   component: Index,
   errorComponent: undefined,
@@ -8,26 +13,54 @@ export const Route = createLazyFileRoute('/')({
 });
 
 function Index() {
+  const links = [
+    {
+      id: 'careers',
+      label: 'Careers',
+      iconSrc: CareersIcon,
+      to: '/careers',
+    },
+    {
+      id: 'projects',
+      label: 'Projects',
+      iconSrc: ProjectsIcon,
+      to: '/til',
+    },
+    {
+      id: 'interview',
+      label: 'Interview',
+      iconSrc: InterviewIcon,
+      to: '/interview',
+    },
+    {
+      id: 'til',
+      label: 'TIL',
+      iconSrc: TILIcon,
+      to: '/til',
+    },
+  ];
+
   return (
-    <div className="max-w-[1000px] mx-auto">
-      <h1 className="text-4xl font-bold text-center">김선민</h1>
-      <ul>
-        <li>
-          <Link to="/interview" className="[&.active]:font-bold">
-            인터뷰
-          </Link>
-        </li>
-        <li>
-          <Link to="/careers" className="[&.active]:font-bold">
-            경력
-          </Link>
-        </li>
-        <li>
-          <Link to="/til" className="[&.active]:font-bold">
-            TIL
-          </Link>
-        </li>
+    <main className="mx-auto px-4 py-6 w-full h-screen break-all pb-10 bg-gradient-to-tr from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90%">
+      <ul className="grid grid-cols-[repeat(auto-fit,minmax(64px,1fr))] gap-4">
+        {links.map(({ id, label, iconSrc, to }) => (
+          <li key={id}>
+            <Link
+              to={to}
+              className="flex min-w-16 flex-col justify-center items-center gap-1 active:transform active:duration-300 active:scale-150 active:opacity-80 hover:opacity-90"
+            >
+              <div className="size-16 bg-slate-400 rounded-xl">
+                <img
+                  src={iconSrc}
+                  alt={`${label}-icon`}
+                  className="rounded-xl"
+                />
+              </div>
+              <span className="text-[12px] text-white">{label}</span>
+            </Link>
+          </li>
+        ))}
       </ul>
-    </div>
+    </main>
   );
 }
