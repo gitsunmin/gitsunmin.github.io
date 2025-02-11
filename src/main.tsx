7ab4f248e0eede162764f88a2017e3fde8bc3500
김선민 <tjsals116@gmail.com>
2025-02-11 13:54:59 +0900

    ⬆️ upgrade dependencies(tailwind-merge, tanstack ...)

--- src/main.tsx
+++ src/main.tsx
@@ -5,13 +5,10 @@ import './globals.css';
 
 import { routeTree } from './routeTree.gen';
 
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { DefaultPending } from '@/components/DefaultPending';
 import { DefaultError } from './components/DefaultError';
 import { Default404 } from './components/Default404';
 
-const queryClient = new QueryClient();
 
 const router = createRouter({
   routeTree,
@@ -30,15 +27,12 @@ if (!rootElement.innerHTML) {
   const root = ReactDOM.createRoot(rootElement);
   root.render(
     <StrictMode>
-      <QueryClientProvider client={queryClient}>
         <RouterProvider
           router={router}
           defaultErrorComponent={DefaultError}
           defaultNotFoundComponent={Default404}
           defaultPendingComponent={DefaultPending}
         />
-        <ReactQueryDevtools initialIsOpen={true} client={queryClient} />
-      </QueryClientProvider>
     </StrictMode>
   );
 }
