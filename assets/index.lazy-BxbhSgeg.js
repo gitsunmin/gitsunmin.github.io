c2d5286e63d00ec65235e46284157bea69f439c2
김선민 <tjsals116@gmail.com>
2025-02-12 23:18:22 +0900

    Updates

--- assets/index.lazy-BxbhSgeg.js
+++ assets/index.lazy-BxbhSgeg.js
@@ -1,4 +1,4 @@
-import{c as a,a as r,j as e,b as c,L as n}from"./index-R1Fyc21V.js";/**
+import{c as a,a as r,j as e,b as c,L as n}from"./index-BRWVivea.js";/**
  * @license lucide-react v0.475.0 - ISC
  *
  * This source code is licensed under the ISC license.
