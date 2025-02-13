68416f0bbafc6e70c474de5d8fa6b4335bba2eef
김선민 <tjsals116@gmail.com>
2025-02-13 19:23:13 +0900

    ✨ MDX files available

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 import path from 'node:path'
 import { copyFileSync } from 'node:fs'
+import mdx from '@mdx-js/rollup';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
@@ -10,37 +11,38 @@ export default defineConfig(({ mode }) => {
     define: {
       '__MODE__': JSON.stringify(mode),
     },
-  plugins: [  
-    TanStackRouterVite({
-      apiBase: '/i/',
-    }),
-    react(),
-    {
-      name: 'generate-404-html', // 플러그인 이름
-      apply: 'build', // build 단계에서 실행
-      closeBundle() {
-        try {
-          copyFileSync('dist/index.html', 'dist/404.html');
-          console.log('✅ 404.html created successfully');
-        } catch (error) {
-          console.error('❌ Failed to create 404.html:', error);
-        }
+    plugins: [
+      TanStackRouterVite({
+        apiBase: '/i/',
+      }),
+      mdx(),
+      react(),
+      {
+        name: 'generate-404-html', // 플러그인 이름
+        apply: 'build', // build 단계에서 실행
+        closeBundle() {
+          try {
+            copyFileSync('dist/index.html', 'dist/404.html');
+            console.log('✅ 404.html created successfully');
+          } catch (error) {
+            console.error('❌ Failed to create 404.html:', error);
+          }
+        },
+      },
+    ],
+    assetsInclude: ['**/*.md'],
+    base: '/i/',
+    server: {
+      watch: {
+        usePolling: true,
+        interval: 100,
       },
     },
-  ],
-  assetsInclude: ['**/*.md'],
-  base: '/i/',
-  server: {
-    watch: {
-      usePolling: true,
-      interval: 100,
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+        "@til": path.resolve(__dirname, "./modules/til"),
+      },
     },
-  },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-      "@til": path.resolve(__dirname, "./modules/til"),
-    },
-  },
-}
+  }
 });
