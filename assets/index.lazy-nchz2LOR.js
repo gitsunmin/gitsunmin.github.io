ddbc36189dee30872a5fe187b3523008d14c70c0
김선민 <tjsals116@gmail.com>
2025-02-16 03:25:20 +0900

    Updates

--- assets/index.lazy-nchz2LOR.js
+++ assets/index.lazy-nchz2LOR.js
@@ -1,4 +1,4 @@
-import{c as a,a as r,j as e,b as c,L as n}from"./index-CzemY5oZ.js";/**
+import{c as a,a as r,j as e,b as c,L as n}from"./index-CXBxzhBu.js";/**
  * @license lucide-react v0.475.0 - ISC
  *
  * This source code is licensed under the ISC license.
