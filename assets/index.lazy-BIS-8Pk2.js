5dcb8f5d7d528df1bb5bb541463fca4611e7babf
김선민 <tjsals116@gmail.com>
2025-02-19 00:57:44 +0900

    Updates

--- assets/index.lazy-BIS-8Pk2.js
+++ assets/index.lazy-BIS-8Pk2.js
@@ -1,4 +1,4 @@
-import{c as a,a as r,j as e,b as c,L as n}from"./index-CXBxzhBu.js";/**
+import{c as a,a as r,j as e,b as c,L as n}from"./index-Df00fZex.js";/**
  * @license lucide-react v0.475.0 - ISC
  *
  * This source code is licensed under the ISC license.
