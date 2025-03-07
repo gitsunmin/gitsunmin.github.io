b65fd3f04eaecf198240503678b9fc70b9d52517
김선민 <tjsals116@gmail.com>
2025-03-07 23:56:09 +0900

    ✨ added Link to my github

--- src/routes/index.lazy.tsx
+++ src/routes/index.lazy.tsx
@@ -7,14 +7,13 @@ import {
   MessageSquareCode,
   MonitorSmartphone,
 } from 'lucide-react';
+import { match } from 'ts-pattern';
 
 export const Route = createLazyFileRoute('/')({
   component: RouteComponent,
 });
 
-type AppLink = (typeof LINKS)[number];
-
-const AppIcon = (props: AppLink) => {
+const InnerLink = (props: InnerLink) => {
   const { to, icon, label, status } = props;
 
   const disabled = status === 'CONSTRUCTION';
@@ -51,8 +50,68 @@ const AppIcon = (props: AppLink) => {
   );
 };
 
-const LINKS = [
+const OuterLink = (props: OuterLink) => {
+  const { href, icon, label, status } = props;
+
+  const disabled = status === 'CONSTRUCTION';
+
+  return (
+    <Link
+      to={href}
+      disabled={disabled}
+      from="/"
+      target="_blank"
+      className={cn('flex w-14 flex-col justify-center items-center gap-1', {
+        'cursor-not-allowed': disabled,
+        'active:transform active:duration-300 active:scale-150 active:opacity-80 hover:opacity-90':
+          !disabled,
+      })}
+    >
+      <div
+        className={cn(
+          'size-14 bg-background rounded-xl shadow-md flex items-center justify-center',
+          {
+            'bg-gray-200 opacity-50': disabled,
+          }
+        )}
+      >
+        {icon}
+      </div>
+      {status === 'CONSTRUCTION' && <div className="absolute text-5xl">🚧</div>}
+      <span
+        className={cn('text-[12px] text-foreground', {
+          'line-through': disabled,
+        })}
+      >
+        {label}
+      </span>
+    </Link>
+  );
+};
+
+type InnerLink = {
+  __type: 'INNER';
+  id: string;
+  label: string;
+  to: string;
+  status: 'ENABLED' | 'CONSTRUCTION';
+  icon: React.ReactNode;
+};
+
+type OuterLink = {
+  __type: 'OUTER';
+  id: string;
+  href: string;
+  status: 'ENABLED' | 'CONSTRUCTION';
+  label: string;
+  icon: React.ReactNode;
+};
+
+type Link = InnerLink | OuterLink;
+
+const LINKS: Link[] = [
   {
+    __type: 'INNER',
     id: 'careers',
     label: 'Careers',
     icon: <Building2 size={28} className="text-foreground" />,
@@ -60,6 +119,7 @@ const LINKS = [
     to: '/careers',
   },
   {
+    __type: 'INNER',
     id: 'projects',
     label: 'Projects',
     icon: <MonitorSmartphone size={28} className="text-foreground" />,
@@ -67,6 +127,7 @@ const LINKS = [
     to: '/projects',
   },
   {
+    __type: 'INNER',
     id: 'interview',
     label: 'Interview',
     icon: <MessageSquareCode size={28} className="text-foreground" />,
@@ -74,11 +135,27 @@ const LINKS = [
     to: '/interview',
   },
   {
+    __type: 'INNER',
     id: 'til',
     label: 'TIL',
     icon: <FileText size={28} className="text-foreground" />,
     status: 'ENABLED',
     to: '/til',
+  },
+  {
+    __type: 'OUTER',
+    id: 'github',
+    label: 'Github',
+    icon: (
+      <img
+        src="https://simpleicons.org/icons/github.svg"
+        alt="github"
+        width={28}
+        height={28}
+      />
+    ),
+    status: 'ENABLED',
+    href: 'https://github.com/gitsunmin',
   },
 ] as const;
 
@@ -97,7 +174,10 @@ function RouteComponent() {
             key={link.id}
             className="flex flex-col justify-center items-center"
           >
-            <AppIcon {...link} />
+            {match(link)
+              .with({ __type: 'INNER' }, (link) => <InnerLink {...link} />)
+              .with({ __type: 'OUTER' }, (link) => <OuterLink {...link} />)
+              .exhaustive()}
           </li>
         ))}
       </ul>
