e7ecb2c32a07bb17f4fa06f6205ca70bc72612a1
김선민 <tjsals116@gmail.com>
2025-03-13 20:36:53 +0900

    ✨ added label on Page Component of projcets

--- src/components/project/Page.tsx
+++ src/components/project/Page.tsx
@@ -3,18 +3,33 @@ import { cn } from '@/lib/utils';
 type Props = React.PropsWithChildren<{
   className?: string;
   variant?: 'default' | 'cover';
+  label?: React.ReactNode;
 }>;
 
-export const Page = ({ variant = 'default', className, children }: Props) => (
+export const Page = ({
+  label = '',
+  variant = 'default',
+  className,
+  children,
+}: Props) => (
   <section
     className={cn(
-      'max-w-sm md:max-w-screen-md lg:max-w-screen-lg mx-auto px-4 snap-always snap-start min-h-[100dvh] py-12 break-words',
+      'w-full md:max-w-screen-md mx-auto px-4 snap-always snap-start min-h-[100dvh] break-words',
       {
         'flex items-center justify-center': variant === 'cover',
       },
       className
     )}
   >
+    <div
+      className={cn('h-12', {
+        'px-4 py-1 rounded-xl flex items-center ml-8 md:ml-0 md:px-0 text-md md:text-2xl font-bold':
+          variant === 'default' && label !== '',
+      })}
+    >
+      {label}
+    </div>
+
     {children}
   </section>
 );
