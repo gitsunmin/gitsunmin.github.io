e2579c3bd3bdead8b16ed85b0e2b5e8cabd84926
김선민 <tjsals116@gmail.com>
2025-03-21 23:14:25 +0900

    ♻️ update construction mark size on main page

--- src/routes/index.lazy.tsx
+++ src/routes/index.lazy.tsx
@@ -1,49 +1,49 @@
-import { createLazyFileRoute, Link } from "@tanstack/react-router";
+import { createLazyFileRoute, Link } from '@tanstack/react-router';
 
-import { cn } from "@/lib/utils";
+import { cn } from '@/lib/utils';
 import {
   Building2,
   FileText,
   MessageSquareCode,
   MonitorSmartphone,
-} from "lucide-react";
+} from 'lucide-react';
 
-export const Route = createLazyFileRoute("/")({
+export const Route = createLazyFileRoute('/')({
   component: RouteComponent,
 });
 
 const AppLink = (props: AppLinkSchema) => {
   const { to, icon, label, target, status } = props;
 
-  const disabled = status === "CONSTRUCTION";
+  const disabled = status === 'CONSTRUCTION';
 
   return (
     <Link
       to={to}
       disabled={disabled}
       target={target}
-      from={"/"}
-      rel={target === "_blank" ? "noopener noreferrer" : undefined}
-      className={cn("flex w-14 flex-col justify-center items-center gap-1", {
-        "cursor-not-allowed": disabled,
-        "active:transform active:duration-300 active:scale-150 active:opacity-80 hover:opacity-90":
+      from={'/'}
+      rel={target === '_blank' ? 'noopener noreferrer' : undefined}
+      className={cn('flex w-14 flex-col justify-center items-center gap-1', {
+        'cursor-not-allowed': disabled,
+        'active:transform active:duration-300 active:scale-150 active:opacity-80 hover:opacity-90':
           !disabled,
       })}
     >
       <div
         className={cn(
-          "size-14 bg-background rounded-xl shadow-md flex items-center justify-center",
+          'size-14 bg-background rounded-xl shadow-md flex items-center justify-center',
           {
-            "bg-gray-200 opacity-50": disabled,
+            'bg-gray-200 opacity-50': disabled,
           }
         )}
       >
         {icon}
       </div>
-      {status === "CONSTRUCTION" && <div className="absolute text-5xl">🚧</div>}
+      {status === 'CONSTRUCTION' && <div className="absolute text-xl">🚧</div>}
       <span
-        className={cn("text-[12px] text-foreground", {
-          "line-through": disabled,
+        className={cn('text-[12px] text-foreground', {
+          'line-through': disabled,
         })}
       >
         {label}
@@ -53,56 +53,56 @@ const AppLink = (props: AppLinkSchema) => {
 };
 
 type AppLinkSchema = {
-  __type: "INNER" | "OUTER";
+  __type: 'INNER' | 'OUTER';
   id: string;
   label: string;
   to: string;
-  target: "_blank" | "_self" | "_parent" | "_top";
-  status: "ENABLED" | "CONSTRUCTION";
+  target: '_blank' | '_self' | '_parent' | '_top';
+  status: 'ENABLED' | 'CONSTRUCTION';
   icon: React.ReactNode;
 };
 
 const LINKS: AppLinkSchema[] = [
   {
-    __type: "INNER",
-    id: "careers",
-    label: "Careers",
+    __type: 'INNER',
+    id: 'careers',
+    label: 'Careers',
     icon: <Building2 size={28} className="text-foreground" />,
-    status: "ENABLED",
-    target: "_self",
-    to: "/careers",
+    status: 'ENABLED',
+    target: '_self',
+    to: '/careers',
   },
   {
-    __type: "INNER",
-    id: "projects",
-    label: "Projects",
+    __type: 'INNER',
+    id: 'projects',
+    label: 'Projects',
     icon: <MonitorSmartphone size={28} className="text-foreground" />,
-    status: "CONSTRUCTION",
-    target: "_self",
-    to: "/projects",
+    status: 'CONSTRUCTION',
+    target: '_self',
+    to: '/projects',
   },
   {
-    __type: "INNER",
-    id: "interview",
-    label: "Interview",
+    __type: 'INNER',
+    id: 'interview',
+    label: 'Interview',
     icon: <MessageSquareCode size={28} className="text-foreground" />,
-    status: "ENABLED",
-    target: "_self",
-    to: "/interview",
+    status: 'ENABLED',
+    target: '_self',
+    to: '/interview',
   },
   {
-    __type: "INNER",
-    id: "til",
-    label: "TIL",
+    __type: 'INNER',
+    id: 'til',
+    label: 'TIL',
     icon: <FileText size={28} className="text-foreground" />,
-    status: "ENABLED",
-    target: "_self",
-    to: "/til",
+    status: 'ENABLED',
+    target: '_self',
+    to: '/til',
   },
   {
-    __type: "OUTER",
-    id: "github",
-    label: "Github",
+    __type: 'OUTER',
+    id: 'github',
+    label: 'Github',
     icon: (
       <img
         src="https://simpleicons.org/icons/github.svg"
@@ -111,18 +111,18 @@ const LINKS: AppLinkSchema[] = [
         height={28}
       />
     ),
-    target: "_blank",
-    status: "ENABLED",
-    to: "https://github.com/gitsunmin",
+    target: '_blank',
+    status: 'ENABLED',
+    to: 'https://github.com/gitsunmin',
   },
   {
-    __type: "OUTER",
-    id: "linkedin",
-    label: "linkedin",
+    __type: 'OUTER',
+    id: 'linkedin',
+    label: 'linkedin',
     icon: <div className="text-foreground font-bold text-3xl">In</div>,
-    target: "_blank",
-    status: "ENABLED",
-    to: "https://www.linkedin.com/in/gitsunmin/",
+    target: '_blank',
+    status: 'ENABLED',
+    to: 'https://www.linkedin.com/in/gitsunmin/',
   },
 ] as const;
 
@@ -130,9 +130,9 @@ function RouteComponent() {
   return (
     <main
       className={cn(
-        "fixed",
-        "mx-auto px-4 py-6 w-full h-[100dvh] break-all pb-10",
-        "bg-background"
+        'fixed',
+        'mx-auto px-4 py-6 w-full h-[100dvh] break-all pb-10',
+        'bg-background'
       )}
     >
       <ul className="grid grid-cols-[repeat(auto-fit,minmax(64px,1fr))] gap-4">
