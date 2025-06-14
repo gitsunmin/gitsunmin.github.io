c3273f580480bfcacef4854a9794c7067bf35578
김선민 <tjsals116@gmail.com>
2025-06-14 23:50:12 +0900

    💄 update til page description text color

--- src/components/pages/TIL.tsx
+++ src/components/pages/TIL.tsx
@@ -2,7 +2,12 @@ type Props = React.PropsWithChildren;
 
 export const TILPage = ({ children }: Props) => (
   <div className="px-6 pt-4 md:px-0">
-    <h1 className="text-5xl font-bold mb-12">TIL (=Today I Learned)</h1>
+    <h1 className="text-5xl font-bold mb-12 w-full">
+      TIL
+      <div className="text-xl font-normal text-gray-500 dark:text-gray-400">
+        (=Today I Learned)
+      </div>
+    </h1>
     {children}
   </div>
 );
