f371fa320a72833988a16ed3b4a0507679657731
김선민 <tjsals116@gmail.com>
2025-06-14 23:12:47 +0900

    ✨ add TIL tile on til page

--- src/components/pages/TIL.tsx
+++ src/components/pages/TIL.tsx
@@ -1,5 +1,8 @@
 type Props = React.PropsWithChildren;
 
 export const TILPage = ({ children }: Props) => (
-  <div className="px-6 pt-4 md:px-0">{children}</div>
+  <div className="px-6 pt-4 md:px-0">
+    <h1 className="text-5xl font-bold mb-12">TIL (=Today I Learned)</h1>
+    {children}
+  </div>
 );
