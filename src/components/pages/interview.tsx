9e7a7d326c3b2cc0803fb1a81c96241586fc5d75
김선민 <tjsals116@gmail.com>
2025-06-19 20:11:38 +0900

    ✨ add a link for additional questions on interview page

--- src/components/pages/interview.tsx
+++ src/components/pages/interview.tsx
@@ -14,14 +14,14 @@ type Props = {
 
 const Content = ({ interviewer, interviewee }: Props) => {
   return (
-    <article className="flex flex-col text-gray-900 dark:text-gray-100 px-4 w-full md:max-w-(--breakpoint-md) mx-auto">
+    <article className="flex flex-col text-gray-900 dark:text-gray-100 px-4 w-full md:max-w-(--breakpoint-md) mx-auto pb-10">
       {INTERVIEW.banner ? (
         <p
           className={twMerge(
             'text-sm break-words',
             'text-gray-500 dark:text-gray-400 border-l-2',
             'border-gray-300 dark:border-gray-600 px-4 py-2 bg-gray-50 dark:bg-gray-800',
-            'mt-4 rounded-lg ',
+            'mt-4 rounded-lg'
           )}
         >
           {INTERVIEW.banner}
@@ -70,6 +70,18 @@ const Content = ({ interviewer, interviewee }: Props) => {
             </div>
           );
         })}
+      </section>
+      <section className="mt-8">
+        <a
+          href="https://github.com/gitsunmin/gitsunmin.github.io/issues/new?template=new_interview_questions.md"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="w-full px-4 py-2 flex items-center justify-center text-sm text-gray-500 dark:text-gray-400 bg-gray-50 dark:bg-gray-800 border border-gray-200 dark:border-gray-700 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700 transition-colors animate-bounce"
+        >
+          <span className="font-bold">추가질문</span>을 하고 싶다면,
+          <span className="font-bold pl-1">클릭</span>
+          <span className="pl-1">해주세요.</span>
+        </a>
       </section>
     </article>
   );
