ec7ae7cd7e2bf35219010f11eb3bd02cce1e2c47
gitsunmin <tjsals116@gmail.com>
2026-03-06 03:35:24 +0900

    feat: SEO 고도화 작업

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import mdx from '@astrojs/mdx';
 import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
 
@@ -7,7 +8,7 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   site: 'https://gitsunmin.github.io',
   base: '/',
-  integrations: [react(), mdx()],
+  integrations: [react(), mdx(), sitemap()],
   vite: {
     plugins: [tailwindcss()],
     resolve: {
